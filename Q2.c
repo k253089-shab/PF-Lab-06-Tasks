975da4e61ebd296e7bb87e5c9d3514dc97c44cf9
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-10-08 02:09:19 +0500

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num1, num2, product=0;
+    
+    printf("Enter first number: ");
+    scanf("\n%d", &num1);
+    printf("Enter second number: ");
+    scanf("\n%d", &num2);
+    
+    for(i=1; i<=num2; i++){
+        product = product + num1;
+    }
+    
+    
+    int divide=num1;
+    int quotient=0;
+    while(divide<=num1 && divide>0){
+        divide = divide - num2;
+        quotient++;
+    }
+    
+    printf("\n%d", product);
+    printf("\n%d", quotient);
+
+    return 0;
+}
