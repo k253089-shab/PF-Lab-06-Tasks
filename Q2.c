a906aed07ed4a3809a9964c7902ecd4c994cc3e4
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-10-09 23:38:06 +0500

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -5,24 +5,31 @@ int main()
     int i, num1, num2, product=0;
     
     printf("Enter first number: ");
-    scanf("\n%d", &num1);
+    scanf("%d", &num1);
     printf("Enter second number: ");
-    scanf("\n%d", &num2);
+    scanf("%d", &num2);
     
     for(i=1; i<=num2; i++){
         product = product + num1;
     }
     
-    
+    if(num2 == 0){
+        printf("\nError: Division by zero is not allowed.");
+        return 0;
+    }    
+        
     int divide=num1;
     int quotient=0;
-    while(divide<=num1 && divide>=num2){
+    
+    while(divide>=num2){
         divide = divide - num2;
         quotient++;
     }
     
+    int remainder = divide;
+    
     printf("\nMultiplication result: %d", product);
-    printf("\nDivision result: %d", quotient);
+    printf("\nDivision result:-\nQuotient: %d\nRemainder: %d", quotient, remainder);
 
     return 0;
 }
